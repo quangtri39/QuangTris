b716858f1bf7496c1c4da03f8ae7754500e9be53
Trí Dương <quangtri222139@gmail.com>
2021-03-14 15:29:23 +0700

    Thêm code cho con ngựa

--- chess.js
+++ chess.js
@@ -72,6 +72,13 @@ document.addEventListener("dragstart", function(event) {
         row: parentDragg.getAttribute("row"), 
         column: parentDragg.getAttribute("column")
     }
+    switch(playerSelect.piece){
+        case "knight":
+            showKnightMove(parentDragg.getAttribute("row"),parentDragg.getAttribute("column"));
+            break;
+        default:
+            console.log(playerSelect.piece);
+    }
     // Cho màu cờ nhạt đi
     event.target.style.opacity = .5;
 }, false);
@@ -89,54 +96,60 @@ document.addEventListener("dragover", function(event) {
 }, false);
 
 // Hàm giúp đổi màu nền những vị trí mà kéo vô
-document.addEventListener("dragenter", function(event) {    
-    // Lấy ô trêm bàm cờ
-    let spot = getParentByClass(event.target,'dropzone');
-    // Nếu người chơi trỏ không đúng ô thì return
-    if(!spot){
-        return;
-    }
-    // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu trùng thì return
-    if(spot.firstChild?.getAttribute("player") == playerSelect.player){
-        return;
-    }
-    // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu không thì chuyển background thành màu hoverKillColor
-    if(spot.firstChild && spot.firstChild?.getAttribute("player") != playerSelect.player) {        
-        event.target.style.background = hoverKillColor;
-        return;
-    }
-    event.target.style.background = hoverColor;
+document.addEventListener("dragenter", function(event) {   
+    
+    event.target.style.opacity = .3;
+    
+    // // Lấy ô trêm bàm cờ
+    // let spot = getParentByClass(event.target,'dropzone');
+    // // Nếu người chơi trỏ không đúng ô thì return
+    // if(!spot){
+    //     return;
+    // }
+    // // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu trùng thì return
+    // if(spot.firstChild?.getAttribute("player") == playerSelect.player){
+    //     return;
+    // }
+    // // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu không thì chuyển background thành màu hoverKillColor
+    // if(spot.firstChild && spot.firstChild?.getAttribute("player") != playerSelect.player) {        
+    //     event.target.style.background = hoverKillColor;
+    //     return;
+    // }
+    // event.target.style.background = hoverColor;
 }, false);
 
 // Hàm khi kéo cờ ra vị trí khác thì trả lại màu nền ban đầu
-document.addEventListener("dragleave", function(event) {    
-    // Lấy ô trêm bàm cờ
-    let spot = getParentByClass(event.target,'dropzone');
-    // Nếu người chơi trỏ không đúng ô thì return
-    if(!spot){
-        return;
-    }
-    event.target.style.background = "";
+document.addEventListener("dragleave", function(event) {   
+    
+    event.target.style.opacity = "";
+    // // Lấy ô trêm bàm cờ
+    // let spot = getParentByClass(event.target,'dropzone');
+    // // Nếu người chơi trỏ không đúng ô thì return
+    // if(!spot){
+    //     return;
+    // }
+    // event.target.style.background = "";
 }, false);
 
 // Hàm thả quân cờ
 document.addEventListener("drop", function(event) {
-    // prevent default action (open as link for some elements)
-    event.preventDefault();
-    // Lấy ô trêm bàm cờ
-    let spot = getParentByClass(event.target,'dropzone');
-    // Nếu người chơi trỏ không đúng ô thì return
-    if(!spot){
-        return;
-    }
-    // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu trùng thì return
-    if(spot.firstChild?.getAttribute("player") == playerSelect.player){
-        return;
-    }
-    // Nếu đến được đây nghĩa là người chơi kéo cờ vào ô trống hoặc ô đã có quân cờ đối thủ
     
-    // Reset màu background lại như cũ
-    event.target.style.background = "";
+    // // prevent default action (open as link for some elements)
+    // event.preventDefault();
+    
+    // Lấy ô trêm bàm cờ
+    if(!isPlaceable(event.target)){
+        event.preventDefault();
+        // Xóa background cho bàn cờ cái này không đặt lên trước được 
+        // tại vì isPlaceable check xem ô có background thì mới kiểm tra
+        clearBoardBackground();
+        return;
+    }
+    // Xóa background cho bàn cờ
+    clearBoardBackground();
+
+    // Lấy cha của đối tượng được Drop cờ xuống
+    let spot = getParentByClass(event.target,'dropzone');    
     // Xóa cờ đối thủ
     spot.innerText = '';
     // Xóa vị trí cũ cờ của mình
@@ -144,6 +157,47 @@ document.addEventListener("drop", function(event) {
     // Thêm cờ của mình vào vị trí mới
     spot.appendChild( playerSelect.dragged );
 }, false);
+
+function clearBoardBackground(){
+    // Lấy tất cả các ô trên bàn cờ rồi reset background
+    let spots = document.querySelectorAll('[row]');
+    // Reset màu background lại như cũ
+    spots.forEach(item =>{
+        item.style.background = "";        
+        item.style.opacity = "";
+    });
+    
+    spots = document.querySelectorAll('.chess');
+    spots.forEach(item =>{
+        item.style.background = "";        
+        item.style.opacity = "";
+    });
+}
+
+function isMyChess(target){
+    let spot = getParentByClass(target,'dropzone');
+    // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu trùng thì return false
+    if(spot.firstChild?.getAttribute("player") == playerSelect.player){
+        return true;
+    }
+    return false;
+}
+
+function isPlaceable(target){
+    let spot = getParentByClass(target,'dropzone');
+    if(!spot){
+        return;
+    }
+    // kiểm tra quân cờ ở ô đó có phải quân cờ của mình không, nếu trùng thì return false
+    if(isMyChess(target)){
+        return false;
+    }
+    // Kiểm tra người dùng có đặt đúng vào ô không
+    if(spot.style.background == ""){
+        return;
+    }
+    return true;
+}
 
 function getParentByClass(element, className){
     // Nếu element đó có tên class thì trả về
@@ -159,4 +213,33 @@ function getParentByClass(element, className){
     }
     // Không có thì trả về rỗng
     return null;
+}
+
+function showKnightMove(posX, posY){    
+    // Lấy bàn cờ
+    let board = document.querySelector('#board');
+    for(row = -2; row <= 2; row++){
+        for(col = -2; col <= 2; col++){
+            let x = Math.abs(row);
+            let y = Math.abs(col);
+            if(x * y == 2){
+                let destinationX = parseInt(posX) + parseInt(row);
+                let destinationY = parseInt(posY) + parseInt(col);
+                var spot = document.querySelector(`[row="${destinationX}"][column="${destinationY}"]`);
+                if(!spot){
+                    continue;
+                }
+                if(spot.firstChild){
+                    if(isMyChess(spot.firstChild)){
+                        spot.style.background = "";
+                        continue;
+                    } else {                
+                        spot.style.background = hoverKillColor;
+                        continue;      
+                    }
+                }
+                spot.style.background = hoverColor;
+            }
+        }
+    }
 }
